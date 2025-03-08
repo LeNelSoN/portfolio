a1a7cec01688c00a72be26dabc3ca64e4d3d0b66
LeNelSoN <nelis.valentin@gmail.com>
2025-03-08 15:44:14 +0100

    font size for navbar

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -4,18 +4,18 @@ const NavBar = () => {
     <aside className="hidden lg:flex flex-col w-64 h-screen text-gray-400 fixed left-0 top-0 p-6">
 
       <div className="mb-10 text-center">
-        <h1 className="text-2xl font-bold text-blue-500">Valentin Nelis</h1>
+        <h1 className="text-lg font-bold text-blue-500">Valentin Nelis</h1>
         <p className="text-gray-400 text-sm">Développeur Web</p>
       </div>
 
       <nav className="flex flex-col space-y-6">
-        <a href="#about" className="flex items-center space-x-3 hover:text-white text-2xl transition">
+        <a href="#about" className="flex items-center space-x-3 hover:text-white text-lg transition">
           <span>À propos</span>
         </a>
-        <a href="#experience" className="flex items-center space-x-3 hover:text-white text-2xl transition">
+        <a href="#experience" className="flex items-center space-x-3 hover:text-white text-lg transition">
           <span>Experience</span>
         </a>
-        <a href="#project" className="flex items-center space-x-3 hover:text-white text-2xl transition">
+        <a href="#project" className="flex items-center space-x-3 hover:text-white text-lg transition">
           <span>Projet</span>
         </a>
       </nav>
