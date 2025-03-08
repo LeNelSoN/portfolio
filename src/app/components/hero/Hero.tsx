c3a571f320cf26129ac66bb03908e68355fb5490
LeNelSoN <nelis.valentin@gmail.com>
2025-03-08 14:43:31 +0100

    exp font size

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       <div className="container mx-auto p-4 md:flex items-center justify-center py-26">
         <div className="mx-auto p-4 text-center md:w-1/2">
           <h2 className="text-4xl mb-6">Le Développement Web</h2>
-          <p className="text-2xl">
+          <p className="md:text-2xl">
             Avec une forte appétence pour la conception d’applications modernes et performantes. 
             J’apprécie particulièrement le travail sur des architectures robustes et scalables, 
             que ce soit en front-end avec Next.js ou en back-end avec Spring Boot. Toujours en quête de nouvelles compétences, 
