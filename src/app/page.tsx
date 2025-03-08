9ad6df3fee9c67f4193373025e326b07e749619c
LeNelSoN <nelis.valentin@gmail.com>
2025-03-08 15:36:54 +0100

    padding bottom fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import ProjectSection from "./components/project-section/ProjectSection";
 export default function Home() {
 
   return (
-    <main className="bg-gray-800 pb-12">
+    <main className="bg-gray-800 pb-12 lg:pb-0">
       <BannerSection id="about"/>
       <Hero id="experience"/>
       <ProjectSection id="project" />
