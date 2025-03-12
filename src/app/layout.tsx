32ad516a662ef6bc7cc79f2dbac8565010e3dcb2
LeNelSoN <nelis.valentin@gmail.com>
2025-03-12 17:26:48 +0100

    webp to jpg preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         {/* Métadonnées pour LinkedIn */}
         <meta property="og:title" content="Découvrez mon Portfolio - Nelis Valentin" />
         <meta property="og:description" content="Un portfolio interactif façon 'Livre dont vous êtes le héros'. Venez explorer mon parcours !" />
-        <meta property="og:image" content="https://nelisvalentin.vercel.app/assets/preview.webp" />
+        <meta property="og:image" content="https://nelisvalentin.vercel.app/assets/preview.jpg" />
         <meta property="og:url" content="https://nelisvalentin.vercel.app/" />
         <meta property="og:type" content="website" />
 
@@ -27,7 +27,7 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Découvrez mon Portfolio - Nelis Valentin" />
         <meta name="twitter:description" content="Un portfolio interactif façon 'Livre dont vous êtes le héros'. Venez explorer mon parcours !" />
-        <meta name="twitter:image" content="https://nelisvalentin.vercel.app/assets/preview.webp" />
+        <meta name="twitter:image" content="https://nelisvalentin.vercel.app/assets/preview.jpg" />
       </head>
       <body
       >
